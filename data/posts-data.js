70514270ccd61bb8bc2e3ff9798e090267ee0d1f
Yang Ke <saint_yk@163.com>
2017-05-02 14:41:11 +0800

    extract posts data to single .js file

---
+++ data/posts-data.js
@@ -0,0 +1,68 @@
+var posts = [
+       {
+            date: "Sep 18 2016",
+            title: "正是虾肥蟹壮时",
+            imgSrc: "/images/post/crab.png",
+            avatar: "/images/avatar/1.png",
+            content: "菊黄蟹正肥，品尝秋之味。徐志摩把,“看初花的荻芦”和“到楼外楼吃蟹”,并列为秋天来杭州不能错过的风雅之事；用林妹妹的话讲是“螯封嫩玉双双满，",
+            reading: "112",
+            collection: "96"
+       },
+       {
+            date: "Nov 20 2016",
+            title: "比利·林恩的中场故事",
+            imgSrc: "/images/post/bl.png",
+            avatar: "/images/avatar/2.png",
+            content: "一 “李安是一位绝不会重复自己的导演，本片将极富原创性李安众所瞩目的新片《比利林恩漫长的中场休息》，正式更名《半场无战事》。",
+            reading: "62",
+            collection: "96"
+       },
+       {
+        title: "当我们在谈论经济学时，我们在谈论什么?",
+        content: "引言在我跟学生课后交流时，以及我在知乎上阅读有关“经济”问题的论题时，经常会遇到这样的情况：...",
+        imgSrc: "/images/post/sls.jpg",
+        reading: 62,
+        collection: 92,
+        author: "知乎",
+        date: "Nov 12 2016",
+        dateTime: "三天前",
+        avatar: "/images/avatar/3.png",
+    },
+    {
+        title: "微信·小程序开发工具安装指南",
+        content: "这两天闲来无事，也安装了 “微信折叠”的开发工具来玩一下。以下是一些小道消息及使用体验，过两天我会写一篇文章以开发者的角度来详细评价微信小程序",
+        imgSrc: "/images/post/xiaolong.jpg",
+        reading: 102,
+        collection: 92,
+        dateTime: "24小时前",
+        author: "猫是猫的猫",
+        date: "Nov 20 2016",
+        avatar: "/images/avatar/5.png"
+    },
+    {
+        title: "从视觉到触觉 这款VR手套能给你真实触感",
+        content: "8月29日消息，据国外媒体VentureBeat报道，一家名为Dexta Robotics的公司最近发布了一款有望变革虚拟现实手部追踪与交互方式的新产品",
+        imgSrc: "/images/post/vr.png",
+        reading: 102,
+        collection: 26,
+        dateTime: "24小时前",
+        author: "深白色",
+        date: "Nov 20 2016",
+        avatar: "/images/avatar/3.png"
+    },
+    {
+        title: "爱奇艺创维开展战略合作，合力布局开放娱乐生态",
+        content: "爱奇艺和创维分别作为国内领先的在线视频品牌",
+        imgSrc: "/images/iqiyi.png",
+        reading: 96,
+        collection: 26,
+        dateTime: "21小时前",
+        author: "深白色",
+        date: "Nov 20 2016",
+        avatar: "/images/avatar/5.png"
+    }
+   ];
+
+module.exports = {
+    postList: posts
+};
--- pages/posts/post.js
+++ pages/posts/post.js
@@ -1,29 +1,13 @@
+var postData = require('../../data/posts-data.js')
+
 Page({
   data:{
   },
   onLoad:function(options){
     // 生命周期函数--监听页面加载
-   var posts = [
-       {
-            date: "Sep 18 2016",
-            title: "正是虾肥蟹壮时",
-            imgSrc: "/images/post/crab.png",
-            avatar: "/images/avatar/1.png",
-            content: "菊黄蟹正肥，品尝秋之味。徐志摩把,“看初花的荻芦”和“到楼外楼吃蟹”,并列为秋天来杭州不能错过的风雅之事；用林妹妹的话讲是“螯封嫩玉双双满，",
-            reading: "112",
-            collection: "96"
-       },
-       {
-            date: "Nov 20 2016",
-            title: "比利·林恩的中场故事",
-            imgSrc: "/images/post/bl.png",
-            avatar: "/images/avatar/2.png",
-            content: "一 “李安是一位绝不会重复自己的导演，本片将极富原创性李安众所瞩目的新片《比利林恩漫长的中场休息》，正式更名《半场无战事》。",
-            reading: "62",
-            collection: "96"
-       }
-   ];
-this.setData({posts});
+    this.setData({
+      posts: postData.postList
+    });
   },
   onReady:function(){
     // 生命周期函数--监听页面初次渲染完成
