54464424c649b985c7998118bd8e7229ed6c50cd
Yang Ke <saint_yk@163.com>
2017-05-13 15:51:11 +0800

    post collected

--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -2,17 +2,80 @@ var postsData = require('../../../data/posts-data.js');
 
 Page({
     onLoad: function(option){
-        var postId = parseInt(option.id);
-        var postDataIndex = postsData.postList.findIndex(function(post){
-           // console.log(post,post.postId,postId);
-            return post.postId == postId;
-        });
+        
+        var postData = 
+            this.fetchPostData(option.id,postsData.postList);
 
-        var postData = postsData.postList[postDataIndex];
-       // console.log(postData);
         // binding data
         this.setData({
             postData: postData
         });
+
+        // set post collected
+        if( !wx.getStorageSync('postsCollected') ){
+            wx.setStorageSync('postsCollected', []);
+        }
+
+        // postIsCollected
+        //console.log(wx.getStorageSync('postsCollected'))
+        var postIsCollected = this.fetchPostData(option.id,wx.getStorageSync('postsCollected'))!=null;
+        this.setData({
+            postIsCollected: postIsCollected
+        });
+
+       // console.log('is collected',postIsCollected);
+       // wx.clearStorageSync();
+    },
+
+    fetchPostData: function(postId,postsList){
+        // exit 0
+        if( postsList.length==0 ) return null;
+        var postId = parseInt(postId);
+        var postDataIndex = postsList.findIndex(function(post){
+            if(!post) return null;
+            return post.postId == postId;
+        });
+
+        if(postDataIndex>-1){
+            return postsList[postDataIndex];
+        }
+    
+        return null;
+    },
+
+    // 收藏
+    onCollectionTap: function(evt){
+        var post = evt.currentTarget.dataset.post;
+        var postId = post.postId;
+        var postsCollected = wx.getStorageSync('postsCollected');
+        console.log("postsCollected",postsCollected);
+
+        // add current post into local storage
+       if( this.fetchPostData(postId,postsCollected)===null){
+           postsCollected.push(post);
+           wx.setStorageSync('postsCollected', postsCollected);
+            this.setData({
+                postIsCollected: true
+            });
+       }
+    },
+
+    // 取消收藏
+    onUnCollectionTap: function(evt){
+        var postId = evt.currentTarget.dataset.postid;
+       
+       // remove current post from local storage
+       var postsCollected = wx.getStorageSync("postsCollected");
+       var idx = postsCollected.findIndex(function(post){
+         return post.postId == postId
+       });
+       //console.log('here',idx);
+       if(idx>-1){
+           postsCollected.splice(idx,1);
+           wx.setStorageSync('postsCollected', postsCollected);
+           this.setData({
+               postIsCollected: false
+           });
+       }
     }
 });
