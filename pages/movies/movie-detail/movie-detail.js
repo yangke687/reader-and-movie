426451613ce03cceaf726f394c25aaa515e67e93
Ke Yang <yangke687@gmail.com>
2017-06-18 22:33:35 +0800

    data binding for movie detail

--- pages/movies/movie-detail/movie-detail.js
+++ pages/movies/movie-detail/movie-detail.js
@@ -16,8 +16,41 @@ Page({
   onLoad: function (options) {
     var movieId = options.id;
     var url = app.globalData.doubanApiPrefix + '/v2/movie/subject/' + movieId;
-    utils.http(url,function(data){
-      console.log(data);
+    utils.http(url,function(res){
+      var data = res.data;
+      // director
+      var director = {
+        avatar: "",
+        name: "",
+        id: ""
+      };
+      if( data.directors[0] !== null){
+        if(data.directors[0].avatars!==null){
+          director.avatar = data.directors[0].avatars.large;
+        }
+        director.name = data.directors[0].name;
+        director.id = data.directors[0].id;
+      }
+      // movie data  
+      var movie = {
+        movieImg: data.images ? data.images.large : '',
+        country: data.countries.join(' '),
+        title: data.title,
+        origTitle: data.original_title,
+        wishCount: data.wish_count,
+        commentCount: data.comment_count,
+        year: data.year,
+        genres: data.genres.join(', '),
+        stars: utils.convertToStarsArray(data.rating.stars),
+        score: data.rating.average,
+        director: director,
+        casts: utils.convertToCastString(data.casts),
+        castsInfo: utils.convertToCastInfos(data.casts),
+        summary: data.summary
+      };
+      this.setData({
+        movie: movie
+      });
     });
   },
 
